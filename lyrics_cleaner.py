6db8244008b7ae5f16c9af108209b5445ef81cb4
Rafat <mohamed@mibrahim.me>
2022-07-03 01:09:16 -0400

    Update lyrics_cleaner to use relative paths

--- lyrics_cleaner.py
+++ lyrics_cleaner.py
@@ -3,12 +3,12 @@ import re
 # Load lyrics file
 file_name = input("Specify filename: ")
 try:
-    file = open(f"C:/rafatDev/deepMumble/Lyrics/{file_name}.txt", "r", encoding="utf-8")
+    file = open(f"./Lyrics/{file_name}.txt", "r", encoding="utf-8")
 except:
     print(f"Could not open the requested file: {file_name}")
 
 # Initialized file where cleaned lyrics will be written
-file_cleaned = open(f"C:/rafatDev/deepMumble/Lyrics/{file_name}_cleaned.txt", "x", encoding="utf-8")
+file_cleaned = open(f"./Lyrics/{file_name}_cleaned.txt", "x", encoding="utf-8")
 for line in file:
     # Remove line that contains song title 
     if 'Lyrics' in line:
